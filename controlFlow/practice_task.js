85183fb03f03a5456c0cd3260590c6f61d1e355c
Abdo Rodriguez <abdo180294@gmail.com>
2025-01-22 18:55:44 -0500

    create practice_task files

---
+++ controlFlow/practice_task.js
@@ -0,0 +1,17 @@
+let authorization;
+let userType = 'employee'
+
+switch(userType) {
+    case 'employee':
+        authorization = 'You are authorized to access Dietary Services';
+        break;
+    case 'enrolled member':
+        authorization = 'You are authorized to access Dietary Services and have one-on-one interaction with a dietician';
+        break;
+    case 'subscriber':
+        authorization = 'You are authorized to access Dietary Services with limited access'
+        break;
+    default:
+        authorization = 'You need to be an enrolled member or subscriber to access Dietary Services';
+        break;
+}
