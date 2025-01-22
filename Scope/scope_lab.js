6954da58ddc20a0dbce12558be4e631011e320e6
Abdo Rodriguez <abdo180294@gmail.com>
2025-01-22 18:39:59 -0500

    create control_flow.html and control_flow.js

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -18,3 +18,15 @@ console.log(blockLet);
 console.log(globalVar); // Output: "I'm a global variable"
 console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
+
+function show(){
+var functionVar = "I'm a block-scoped var";
+let functionLet = "I'm a block-scoped let";
+const functionConst = "I'm a block-scoped const";
+
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); // Throws ReferenceError
+}
+show();
+
